9f106a3bc40d7914f50cd3c2ff8b1a6515e748b2
Yar Pi <kinglordlear@gmail.com>
2018-07-01 19:05:33 +0000

    #Debug branch. Idea writen code instead of Sergey). Main algorithm added

---
+++ src-serega/Project/model/Main.java
@@ -0,0 +1,10 @@
+package Project.model;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Network graph = new Network();
+        graph.Transite();
+        graph.write();
+    }
+}
